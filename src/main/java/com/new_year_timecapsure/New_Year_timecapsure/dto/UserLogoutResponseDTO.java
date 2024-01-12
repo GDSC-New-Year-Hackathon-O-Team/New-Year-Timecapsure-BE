b9487f8f0a6f03e247b379908d98d4e64bf5f0b3
Hwangseoeun <sunny030921@gmail.com>
2024-01-13 01:36:58 +0900

    UserLogoutDTO

--- src/main/java/com/new_year_timecapsure/New_Year_timecapsure/dto/UserLogoutResponseDTO.java
+++ src/main/java/com/new_year_timecapsure/New_Year_timecapsure/dto/UserLogoutResponseDTO.java
@@ -1,4 +1,7 @@
 package com.new_year_timecapsure.New_Year_timecapsure.dto;
 
+import lombok.Setter;
+
 public class UserLogoutResponseDTO {
+    private boolean isLogout;
 }
--- src/main/java/com/new_year_timecapsure/New_Year_timecapsure/dto/UserSignupRequestDTO.java
+++ src/main/java/com/new_year_timecapsure/New_Year_timecapsure/dto/UserSignupRequestDTO.java
@@ -1,11 +1,12 @@
 package com.new_year_timecapsure.New_Year_timecapsure.dto;
 
-import com.new_year_timecapsure.New_Year_timecapsure.entity.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class UserLogoutRequestDTO {
-    private String userId;
+public class UserSignupRequestDTO {
+    private Long userId;
+    private String userEmail;
+    private String password;
 }
