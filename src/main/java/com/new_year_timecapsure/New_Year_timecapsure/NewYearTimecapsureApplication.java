3aaa4a2e709d8d5d776e6737a5b7e06c0791c433
Hwangseoeun <sunny030921@gmail.com>
2024-01-12 21:09:38 +0900

    init commit

---
+++ src/main/java/com/new_year_timecapsure/New_Year_timecapsure/NewYearTimecapsureApplication.java
@@ -0,0 +1,13 @@
+package com.new_year_timecapsure.New_Year_timecapsure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewYearTimecapsureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewYearTimecapsureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/new_year_timecapsure/New_Year_timecapsure/NewYearTimecapsureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.new_year_timecapsure.New_Year_timecapsure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewYearTimecapsureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
